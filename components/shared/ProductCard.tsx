8b4c18ecdcc7f72d85d229828139d6893f56e412
Habibi Gusti Pangestu <habibigustip@gmail.com>
2025-05-08 09:35:29 +0700

    implement truncate text and format price on product card

--- components/shared/ProductCard.tsx
+++ components/shared/ProductCard.tsx
@@ -1,21 +1,25 @@
+import { formatPrice, truncateText } from "@/lib/utils"
 import { Product } from "@/types/product"
 import Image from "next/image"
 
-export default function ProductCard({ products }: { products: Product[] }) {
+export default function ProductCard({ product }: { product: Product }) {
   return (
     <>
-      {products.map((product) => (
-        <div key={product.id} className="border rounded-lg p-4 shadow-sm hover:shadow-md">
-          <div className="relative h-40 bg-purple-100 mb-4 rounded-lg">
-            <Image src={product.image} alt="Car" fill objectFit="cover" className="rounded-lg"/>
-          </div>
-          <h3 className="text-lg font-medium text-gray-900">{product.name}</h3>
-          <p className="text-purple-600 mt-2">{product.price}</p>
-          <button className="mt-4 w-full bg-purple-600 text-white py-2 rounded-lg hover:bg-purple-700">
-            Add to Cart
-          </button>
+      <div className="border min-h-[358px] rounded-lg p-4 shadow-sm hover:shadow-md">
+        <div className="relative h-50 bg-purple-100 mb-4 rounded-lg">
+        <Image
+            src={product.images[0]}
+            alt="Car"
+            fill
+            className="object-cover rounded-lg"
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            priority
+        />
         </div>
-      ))}
+        <h3 className="text-lg font-medium text-gray-900">{truncateText(product.title, 25)}</h3>
+        <p className="text-sm text-gray-600">{truncateText(product.description, 60)}</p>
+        <p className="font-bold mt-2">{formatPrice(product.price)}</p>
+      </div>
     </>
   )
 }
